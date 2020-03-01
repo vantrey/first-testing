4b5399212976c329b5adedcef5f5d67dd53adbf4
Ivan Tolkachev <vantrey@yandex.ru>
2020-03-01 14:15:56 +0300

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import styles from './App.module.css';
 import Counter from "./Counter/Counter";
 
 class App extends React.Component {
-
   state = {
     counter: 0,
     maxCount: 5,
     minCount: 0,
   };
-
   inc = () => {
       if (this.state.counter < this.state.maxCount) {
       this.setState({counter: this.state.counter +1})
